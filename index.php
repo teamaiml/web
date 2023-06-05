dab7baf81d8045532cc19e6330ce45ed2ec3b1ec
teamaiml <134915405+teamaiml@users.noreply.github.com>
2023-06-05 14:39:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>TEAM</title>
+    <link rel="icon" href="TEAM logo.png" type="image/x-icon">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <?php echo '<link href="style.css" rel="stylesheet">'; ?>
+
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="logo">
+                    <img src="img\logo1.png" width="110" height="110">
+
+                    <h1>TEAM</h1>
+                    <h3>TECHNICAL EXCELLENCE IN AI AND ML</h3>
+                </div>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="ingenium.php">INGENIUM 2K23</a></li>
+                    <li><a href="#section1">EVENTS</a></li>
+                    <li><a href="#section2">ABOUT US</a></li>
+                    <li><a href="#footer">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- First Container -->
+  <!--  <video width="3600px" height="1000px" controls loop autoplay muted>
+        <source src="team page video.mp4" type="video/mp4">
+
+    </video> -->
+    <div class="embed-responsive embed-responsive-16by9">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/FCilDM5L-P8?&autoplay=1" title="YouTube video player" frameborder="0"  allow="accelerometer; autoplay;
+    clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+
+    <div class="about-section">
+        <div class="inner-width">
+            <h1>INGENIUM 2K23</h1>
+            <div class="border"></div>
+            <div class="about-section-row">
+                <div class="about-section-col">
+                    <div class="about">
+                        <p>
+                            From June 12th to June 15th, 2023, the Malla Reddy Engineering College will be buzzing
+                            with
+                            energy and excitement as the AIML student community TEAM (Technical Excellence in AI &
+                            ML)
+                            presents INGENIUM 2K23, a four-day technical symposium aimed at celebrating innovation
+                            and
+                            technology.
+                            The event promises to be a thrilling amalgamation of technical knowledge, interactive
+                            workshops, and engaging guest lectures that will bring together the brightest minds from
+                            the
+                            world of technology. INGENIUM 2k23 will provide a platform for students, professionals,
+                            and
+                            enthusiasts to explore the latest trends, exchange ideas, and network with industry
+                            experts.
+
+                        </p>
+                        <a href="ingenium.php">Read More</a>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+        <img src="img\INGENIUM 2K23.png" alt="" width="800" height="500">
+    </div>
+
+    <div class="about-section2" id="section1">
+
+        <h1>TEAM EVENTS</h1>
+        <div class="containe">
+            <div class="wrapper">
+                <img src="img\teamevents1.png">
+                <img src="img\teamevents2.png">
+                <img src="img\teamevents3.png">
+                <img src="img\teamevents4.png">
+                <img src="img\teamevents5.png">
+                <img src="img\teamevents6.png">
+                <img src="img\teamevents7.png">
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Third Container (Grid) -->
+    <div class="about-section1" id="section2">
+        <h1>About Us</h1>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="img\MALLA REDDY.jpg" alt="Jane" style="width:80%">
+                    <div class="contain">
+                        <h2 style="text-align:center">SRI CH.MALLA REDDY</h2>
+                        <p class="title" style="text-align:center">FOUNDER CHAIRMAN, MREC </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card1">
+                    <img src="img\A.Ramaswami Reddy.jpg" alt="Jane" style="width:80%">
+                    <div class="contain">
+                        <h2 style="margin-left:-20px"> DR A.RAMASWAMI REDDY</h2>
+                        <p class="title" style="text-align:center">DIRECTOR / PRINCIPAL MREC (A)
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="img\shivprasad.jpg" alt="Jane" style="width:80%">
+                    <div class="contain">
+                        <h2 style="margin-left:-20px">DR S.SHIVA PRASAD
+                        </h2>
+                        <p class="title" style="text-align:center">HOD Department of CSE(AIML,DS)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="MREC">
+            <h1>About MREC</h1>
+            <h3>Malla Reddy engineering College (MREC) is the parent college of the Malla Reddy Group of
+                Institutions,
+                Hyderabad, Telangana, India founded by Shri Ch Malla Reddy.The institute was established in 2002, is
+                approved by the AICTE New Delhi, and was affiliated to Jawaharlal Nehru Technological University,
+                Hyderabad (JNTUH). In 2008 the college was accredited by NBA.The college has been certified by NAAC
+                as
+                an A++ grade institution in the Hyderabad region.The college has been granted Permanent Affiliation,
+                and
+                Autonomous status by JNTU in 2011.</h3>
+        </div>
+        <div class="TEAM">
+            <h1>About TEAM</h1>
+            <h3>TEAM an student community of Department of CSE- Artificial Intelligence and Machine Learning from
+                Malla Reddy Engineering College. We are a group of students passionate about the exciting and
+                rapidly growing fields of artificial intelligence and machine learning. Our community is open to all
+                students, regardless of their level of expertise or background, interested in learning more about
+                these fields and connecting with like-minded individuals.This community aims to provide a platform
+                for its members to connect, learn, collaborate, and share their knowledge and experiences with each
+                other.
+
+                This Community is open to all students who are studying or interested in pursuing a career in AI and
+                ML.Members of the community come from a variety of backgrounds, including computer science,
+                engineering, mathematics, and other related fields.
+
+                The community provides various resources and activities to its members, including online courses,
+                webinars, workshops, and hackathons. These activities are designed to help members improve their
+                skills and knowledge in AI and ML, and to connect with other like-minded individuals.
+            </h3>
+        </div>
+    </div>
+    <div class="about-section3" id="section3">
+
+        <h1>TEAM MEMBERS</h1>
+        <div class="containe1">
+            <div class="wrapper1">
+                <img src="img\1.png">
+                <img src="img\2.png">
+                <img src="img\3.png">
+                <img src="img\4.png">
+                <img src="img\5.png">
+                <img src="img\6.png">
+                <img src="img\7.png">
+                <img src="img\8.png">
+
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <footer id="footer">
+        <div id="contact" style="font-size: 20px;">TEAM ( TECHNICAL EXCELLENCE IN AI & ML)
+            <div class="social-icons" id="contact" style="font-size: 30px;z">
+            <!-- <h3>Follow us:</h3> -->
+                <a class="socil-icons" href="https://instagram.com/team.mrec?igshid=MzNlNGNkZWQ4Mg==" target="_blank">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a href="https://www.linkedin.com/company/teammrec" target="_blank">
+                    <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a href="mailto:teamcseaiml@mrec.ac.in" target="_blank">
+                    <ion-icon name="mail-outline"></ion-icon>
+                </a>
+                <a href="https://chat.whatsapp.com/CG0bFlA7H8yIniZKgLtrHt" target="_blank">
+                    <ion-icon name="logo-whatsapp"></ion-icon>
+                </a>
+                <div style="font-size: 20px; margin-right: 50px;">Contact us on: team.cseaiml@mrec.ac.in</div>
+                <span>TEAM &copy; | 2023 All rights are reserved.</span>
+                <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+                <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </footer>
+</body>
+
+</html>
+
+
+
+
+
+
+</body>
+
+
+</html>
---
+++ ingenium.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>INGENIUM 2K23</title>
+    <link rel="icon" href="ingeniumlogo.png" type="image/x-icon">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <?php echo '<link href="style1.css" rel="stylesheet">'; ?>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="ingenium.php">INGENIUM</a></li>
+                    <li><a href="#column">EVENTS</a></li>
+                    <li><a href="#coodinators">COORINATORS</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- <video width="3800px" height="1000px" controls loop autoplay muted>
+        <source src="intro.mp4" type="video/mp4">
+
+    </video>  -->
+    <div class="embed-responsive embed-responsive-16by9">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/K8K3VYHTEsw?&autoplay=1" title="YouTube video player" frameborder="0"  allow="accelerometer; autoplay;
+    clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+
+    <!-- Wrapper for slides -->
+    <div class="background1">
+        <div class="matter1">
+            <h1>INGENIUM 2K23</h1>
+            <h3>From June 12th to June 15th, 2023, the Malla Reddy Engineering College will be buzzing with energy and
+                excitement as the AIML student community TEAM (Technical Excellence in AI & ML) presents INGENIUM 2K23,
+                a four-day technical symposium aimed at celebrating innovation and technology.
+                The event promises to be a thrilling amalgamation of technical knowledge, interactive workshops, and
+                engaging guest lectures that will bring together the brightest minds from the world of technology.
+                INGENIUM 2k23 will provide a platform for students, professionals, and enthusiasts to explore the latest
+                trends, exchange ideas, and network with industry experts.
+                With a line-up of events spread across the four days, there will be something for everyone at INGENIUM
+                2k23. Attendees can look forward to a range of technical competitions, including hackathons, coding
+                challenges,as well as interactive workshops on topics like Machine Learning, App Development.
+                The symposium will also feature keynote speeches from some of the most eminent names in the technology
+                industry, providing attendees with a unique opportunity to learn from their experiences and gain
+                insights into the latest developments in the field.
+                In addition to the technical events, INGENIUM 2k23 will also have a range of cultural and entertainment
+                events, and interactive games.
+                The AIML student community TEAM has left no stone unturned in their efforts to make INGENIUM 2k23 a
+                grand success. With meticulous planning and attention to detail, the event promises to be a memorable
+                experience for all attendees.
+                Whether you are a technology enthusiast, a student looking to expand your knowledge, or an industry
+                professional looking to network and explore new opportunities, INGENIUM 2k23 is the place to be. Mark
+                your calendars and join the celebration of innovation and technology at Malla Reddy Engineering College
+                from June 12th to June 15th, 2023.
+            </h3>
+        </div>
+        <div class="item">
+
+            <img src="img\INGENIUM 2K23.png" alt="" width="1200" height="700">
+
+            <div class="buttons-container">
+                <button class="button-arounder"> <a href="https://forms.gle/ihbPfJf4U6QoVV9m9" target="_blank">Register Now</button>
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+    <!-- Container (INGENIUM Section) -->
+
+
+
+    <div class="background2">
+        <div class="MREC">
+            <h1>EVENTS</h1>
+
+        </div>
+    </div>
+    <div class="background3" id="column">
+        <div class="row">
+            <div class="column">
+                <h2>WORKSHOPS</h2>
+                <h3>MACHINE LEARNING</h2>
+                    <div class="para">
+                        <p>Machine learning is a subfield of artificial intelligence (AI) that focuses on the development of algorithms and models that allow computers to learn and make predictions or decisions without being explicitly programmed. It involves designing and implementing systems that can learn from data, identify patterns, and make informed decisions or predictions. A Two-day hands-on Machine learning workshop aims to provide participants with practical experience in applying machine learning techniques to real-world problems.</p>
+                    </div>
+
+                    <h3>POWER-BI</h2>
+                        <div class="para">
+                            <p>Power BI is a business intelligence and data visualization tool developed by Microsoft. It allows users to connect to various data sources, transform and model data, create interactive reports and dashboards, and share insights with others. A Two-day hands-on Power BI workshop aims to provide participants with practical experience in using Power BI to analyze and visualize data.</p>
+                        </div>
+            </div>
+            <div class="column">
+                <h2>EVENTS</h2>
+                <h3>TECHNOFIESTA</h2>
+                    <div class="para">
+                        <p>Coding Crown</p>
+                        <p>ChatGPT Faceoff</p>
+                    </div>
+                    <h3>FUNFILLED FIESTA</h2>
+                        <div class="para">
+                            <p>BGMI Tournament</p>
+                            <p>LolCode</p>
+                            <p>VR Virtual Reality Gaming</p>
+                        </div>
+            </div>
+        </div>
+    </div>
+    <div class="coordinators">
+        <h1>COORDINATORS</h1>
+        <div class="names">
+            <h2>P. Anjani Kumar</h2>
+            <h2>9573608462</h2>
+        </div>
+        <div class="names1">
+            <h2>D. Vignan Sai Ram</h2>
+            <h3>9390548447</h3>
+        </div>
+    </div>
+</body>
